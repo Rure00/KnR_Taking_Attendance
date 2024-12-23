1577d1eb8cb1646e3e98bd2b8e6422b7190604e6
rure00 <asnjkd@naver.com>
2024-12-23 22:21:22 +0900

    add: colors

--- app/src/main/java/com/rure/knr_takingattendance/presentation/MainActivity.kt
+++ app/src/main/java/com/rure/knr_takingattendance/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rure.knr_takingattendance
+package com.rure.knr_takingattendance.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -38,7 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     KnR_TakingAttendanceTheme {
--- app/src/main/java/com/rure/knr_takingattendance/ui/theme/Color.kt
+++ app/src/main/java/com/rure/knr_takingattendance/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val White = Color.White
+val Black = Color.Black
+val TossBlue = Color(0xFF0064FF)
+val TossGray = Color(0xFF202632)
+val Gray = Color(0xFF525050)
+val LightGray = Color(0xFFE2E2E2)
+
+val CautionOrange = Color(0xFFFFAC49)
+val WarningRed = Color(0xFFFF5353)
+
